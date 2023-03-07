bd704fb7d343e2d81d28cb757b63f6769fd51a16
SALL Cheikh tidiane <cheikh-tidiane.sall@alstomgroup.com>
2023-03-07 00:37:47 -0500

    Add first scrpit logic

--- script.js
+++ script.js
@@ -31,3 +31,38 @@ const operate = function(operator,a,b) {
     }
   };
   
+
+  const buttons = document.querySelectorAll('button');
+  let total = 0;
+  let memory = 0;
+  let operator ="";
+  buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+      
+        if (Number.isInteger(parseInt(button.textContent)) && operator == "") { // If the user clicks on a number and no operator is selected, keep it in memory
+            memory = memory + button.textContent;
+        }
+        else if (Number.isInteger(parseInt(button.textContent)) && operator != ""){ // If the user clicks on a number and a operator is selected,compute
+            let result = operate(operator,parseInt(memory),memory,button.textContent);
+            console.log(result);
+        }
+        
+        if (button.textContent =="AC") {
+            memory = "0";
+        }
+        
+        
+        if (button.textContent =="+"|| button.textContent =="-"|| button.textContent =="/" || button.textContent =="x") {
+            operator = button.textContent;
+        }
+          
+      });
+   });
+   
+
+
+  const disp = function() {
+
+
+    
+  }
