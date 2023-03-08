ebf22cf22825c2cb8f540ddabba1e29c750c78fb
SALL Cheikh tidiane <cheikh-tidiane.sall@alstomgroup.com>
2023-03-07 20:18:01 -0500

    Completion of basic features (+-*/=) and AC feature

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Fist we create the functions for the arithmetic
+
 const add = function(a,b) {
 	return a + b ;
 };
@@ -14,7 +16,7 @@ const divide = function(a,b) {
     return a / b;
   };
   
-const operate = function(operator,a,b) {
+const operate = function(operator,a,b) { // Function to compute the numbers with the operator
     if (operator == "+") {
         return add(a,b);
     }
@@ -33,43 +35,60 @@ const operate = function(operator,a,b) {
   
 
   const buttons = document.querySelectorAll('button');
-  let result = 0;
+  let result = null; // initizilation of variables
   let firstNumber = "0";
   let secondNumber = "";
   let operator ="";
 
-  const display = document.querySelector('.display');
+  const display = document.querySelector('.display'); // creation of the text display 
   const numberDisplay = document.createElement('p');
-  numberDisplay.classList.add('numberDisplay');
-  numberDisplay.textContent = result;
+  numberDisplay.textContent = 0; 
   display.appendChild(numberDisplay);
+
+
   buttons.forEach((button) => {
       button.addEventListener('click', () => {
       
-        if (Number.isInteger(parseInt(button.textContent)) && secondNumber == "" && operator == "") { // If the user clicks on a number and no operator is selected, keep it in memory
-            firstNumber = firstNumber + button.textContent;
+        if (Number.isInteger(parseInt(button.textContent)) && secondNumber == "" && operator == "") { // If the user clicks on a number and no operator is selected
+            firstNumber += button.textContent;
             numberDisplay.textContent = parseInt(firstNumber); // convert the display to int to remove the first zero
-            
+            console.log("FirstNumber: " + firstNumber)
+            console.log("SecondNumber: " + secondNumber)
         }
-        if (Number.isInteger(parseInt(button.textContent)) && operator != ""){ // If the user clicks on a number and a operator is selected,compute
-            secondNumber = secondNumber + button.textContent;
+        if (Number.isInteger(parseInt(button.textContent)) && operator != ""){ // If the user clicks on a number and a operator is selected
+            secondNumber += button.textContent;
             numberDisplay.textContent = parseInt(secondNumber); // convert the display to int to remove the first zero
+            console.log("firstNumber: " + firstNumber)
+            console.log("secondNumber: " + secondNumber)
+            console.log("operator: " + operator)
         }
 
-        if (button.textContent =="+"|| button.textContent =="-"|| button.textContent =="/" || button.textContent =="x") {
+        
+
+        if (button.textContent =="+"|| button.textContent =="-"|| button.textContent =="/" || button.textContent =="x") { // If an operator is selected , keep ot in memory
             operator = button.textContent;
-            
+            console.log("FIRSTNumber: " + firstNumber)
+            console.log("SECONDNumber: " + secondNumber)
+            console.log("operator: " + operator)
         }
-        if (button.textContent =="=" && secondNumber != ""){ // If the user clicks on a number and a operator is selected,compute
+        if (button.textContent =="=" && secondNumber != ""){ // If the user clicks on the equal sign  and a second number exists, compute the result
             result = operate(operator,parseInt(firstNumber),parseInt(secondNumber));
             numberDisplay.textContent = result;
-            firstNumber = "";
-            operator == "";
+            firstNumber = result; // the result become the first number and theother variables become null
+            secondNumber = "";
+            operator = "";
+            console.log("FIRSTNUMBER: " + firstNumber)
+            console.log("SECONDNUMBER: " + secondNumber)
+            console.log("operator: " + operator)
         }
-        
-        if (button.textContent =="AC") {
+
+        if (button.textContent =="AC") { // Clears everything
             numberDisplay.textContent = 0;
+            result = null;
             firstNumber = "0";
+            secondNumber = "";
+            operator ="";
+
         }
         
 
